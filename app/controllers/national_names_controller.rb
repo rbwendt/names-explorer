110342e3ca8410e903ace8ea2eb5639b7325175b
Ben Wendt <ben@theworkinggroup.ca>
2016-03-29 20:10:05 -0400

    Make it show multiple names at once

--- app/controllers/national_names_controller.rb
+++ app/controllers/national_names_controller.rb
@@ -15,9 +15,15 @@ class NationalNamesController < ApplicationController
   end
 
   def show
-    @all_names = NationalName.where(:Name => params[:id])
+    @all_names = NationalName.where(:Name => params[:id].split(','))
     unless params[:Gender].blank?
       @all_names = @all_names.where(:Gender => params[:Gender])
+    end
+    @all_names = @all_names.group_by{|a| a.Name}
+
+    @chart_data = @all_names.map do |k,v|
+      base = (1880..2014).map{|i| [i, 0]}.to_h
+      {:name => k, :data => v.reduce(base){|h,g| h[g.Year] = g.Count;h}}
     end
   end
 end
